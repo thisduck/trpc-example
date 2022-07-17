89650fcf01a0b554041683ce93ee26b8ad7ea9fa
Adnan Ali <adnan.ali@gmail.com>
2022-07-16 20:20:17 -0400

    Adding trpc backend.

---
+++ backend/src/server.ts
@@ -0,0 +1,43 @@
+import * as trpc from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
+import { z } from "zod";
+
+const app = express();
+
+const appRouter = trpc
+  .router()
+  .query("getUser", {
+    input: z.string(),
+    async resolve(req) {
+      req.input; // string
+      return { id: req.input, name: "Bilbo" };
+    },
+  })
+  .mutation("createUser", {
+    // validate input with Zod
+    input: z.object({ name: z.string().min(5) }),
+    async resolve(req) {
+      return { id: req.input, name: "Frodo" };
+    },
+  });
+
+// created for each request
+const createContext = ({
+  req: _req,
+  res: _res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+// type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+app.listen(4000);
